c10a19bb029ad4994d9d318284ee3657c6e13459
Kevin Blomquist <knblomquist00@gmail.com>
2022-11-09 09:43:57 -0700

    yes

---
+++ Game/Jumper.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace cse210_03.Game
+{
+    
+    public class Jumper
+    {
+        private List<string> player = new List<string>[ "   ___ ", 
+                                            "  /___\\",
+                                            "  \\   /",                                            \ /
+                                            "    o  ",
+                                            "   /|\\",
+                                            "   / \\ ",
+                                            "^^^^^^^^^"];
+
+        
+    }
+}

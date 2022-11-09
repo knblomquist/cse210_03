8201f792ffc1a900a2f07766fff4d11094ee779c
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2022-11-09 08:59:54 -0700

    change8

--- Game/word.cs
+++ Game/word.cs
@@ -7,7 +7,6 @@ namespace Word.Game
     {   
         private string secretWord;
         private List<char> letterList = new List<char>();
-        private string encodedWord;
         
         public void EncodeWord(string randomWord)
         {
@@ -28,7 +27,7 @@ namespace Word.Game
             }
         }
 
-        public bool UpdateWord(string letter)
+        private bool IsLetterGuessed(string letter)
         {
             // Compare letter to secretWord
             // If letter is in secretWord, replace the underscore with the letter
@@ -37,11 +36,37 @@ namespace Word.Game
             {
                 if (letter == secretWord[i].ToString())
                 {
-                    letterList[i] = letter[0];
                     letterGuessed = true;
                 }
             }
             return letterGuessed;
         }
+
+        public void UpdateWord(string letter)
+        {
+            // Compare letter to secretWord
+            // If letter is in secretWord, replace the underscore with the letter
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                if (letter == secretWord[i].ToString())
+                {
+                    letterList[i] = letter[0];
+                }
+            }
+        }
+        public bool IsWordGuessed()
+        {
+            // Check if the word has been guessed
+            bool wordGuessed = true;
+            for (int i = 0; i < letterList.Count; i++)
+            {
+                if (letterList[i] == '_')
+                {
+                    wordGuessed = false;
+                }
+            }
+            return wordGuessed;
+        }
+        
     }
 }

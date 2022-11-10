83380b537878b87cf209f90e529533975010a6dd
Kevin Blomquist <knblomquist00@gmail.com>
2022-11-10 09:04:23 -0700

    wordfactory

--- Game/WordFactory.cs
+++ Game/WordFactory.cs
@@ -5,11 +5,11 @@ namespace cse210_03
 {
    class WordFactory
    {
-     public List <string> words = new List<string>{"Aa","Kerfuffle", "Mormon", "Catatonic", "Alphabet", "Hamster", "Strawberry"};
-      static void RandomWord()
+     private List <string> words = new List<string>{"Aa","Kerfuffle", "Mormon", "Catatonic", "Alphabet", "Hamster", "Strawberry"};
+      public string RandomWord()
       {
          var newRandom = new Random();
-         int index = Random.Next(words.Count);
+         int index = newRandom.Next(words.Count);
          
          return words[index];
       }
